a3630d92935faf5e8e616e9a4bd5915883b08a49
Shmantsar Angelina <Angelina.Shmantsar@urfu.me>
2025-05-20 21:53:00 +0500

    Сделала моки для тем по CSS и HTML

---
+++ transparent-world/src/mocks/cssThemes.ts
@@ -0,0 +1,44 @@
+import { Theme } from '../models/theme';
+
+export const cssThemes: readonly Theme[] = [
+    {
+        name: 'Основы CSS',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Работа с текстом и шрифтами',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Блочная модель (Box Model)',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Позиционирование и Flexbox',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Grid Layout',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Анимации и трансформации',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Адаптивный дизайн',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'CSS Custom Properties)',
+        theory: [],
+        practice: [],
+    },
+];
---
+++ transparent-world/src/mocks/htmlThemes.ts
@@ -0,0 +1,44 @@
+import { Theme } from '../models/theme';
+
+export const htmlThemes: readonly Theme[] = [
+    {
+        name: 'Основы HTML',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Семантическая разметка',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Формы и интерактивные элементы',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Мультимедиа и графики',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Таблицы',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Доступность (a11y)',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Интеграция с API браузера',
+        theory: [],
+        practice: [],
+    },
+    {
+        name: 'Современные API',
+        theory: [],
+        practice: [],
+    },
+];
