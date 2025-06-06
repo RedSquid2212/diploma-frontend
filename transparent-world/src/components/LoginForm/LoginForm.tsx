af9aab320944cfa9fe1bfce560b4a829a1b4c788
Shmantsar Angelina <Angelina.Shmantsar@urfu.me>
2025-06-06 16:31:59 +0500

    Добавила редирект после логина

--- transparent-world/src/components/LoginForm/LoginForm.tsx
+++ transparent-world/src/components/LoginForm/LoginForm.tsx
@@ -1,21 +1,27 @@
 import { Button } from '@mui/material';
 import { FC, memo, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import './LoginForm.scss';
 import { client } from '../../services/client.service';
+import { User } from '../../models/user';
 
 const LoginFormComponent: FC = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [user, setUser] = useState<User | null>(null);
 
     const handleLoginButtonClick = async () => {
         try {
-            const result = await client.login({ username, password });
-            console.log(result);
+            const result = await client.login({ username, password }) as User;
+            setUser(result);
         } catch (error: unknown) {
             console.error(error);
         }
     };
+
+    if (user) {
+        return <Navigate to={'/'}/>
+    }
 
     return (
         <form className="loginForm" >
